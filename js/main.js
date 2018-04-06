f5b81d35e9aee4440b1f2d5988b2b4212d6011f1
Simon Sun <ssun@college.harvard.edu>
2018-04-06 00:16:48 -0400

    add item page, TABS NOT WORKING (jquery issue?)

--- js/main.js
+++ js/main.js
@@ -16,20 +16,11 @@ var instance = M.Carousel.init(elem, {
 var elem = document.querySelector('.datepicker');
 var instance = M.Datepicker.init(elem);
 
-<<<<<<< HEAD
 // var elemSelectMeal = document.querySelector('select');
 // var instanceSelectMeal = M.FormSelect.init(elemSelectMeal);
-=======
-// var elemSelectMeal = document.querySelector('select');
-// var instanceSelectMeal = M.FormSelect.init(elemSelectMeal, options);
->>>>>>> c3195265d03210100c7295ce03acc8213d4c89c3
 
 
 // var todd = document.querySelector("#today").valueAsDate = new Date();
 
-<<<<<<< HEAD
-
-=======
-
 // var instance_t = M.Tabs.init(el);
->>>>>>> c3195265d03210100c7295ce03acc8213d4c89c3
+
