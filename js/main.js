c3195265d03210100c7295ce03acc8213d4c89c3
Luke Xu <Lukexu@Lukes-MacBook-Pro-5.local>
2018-04-05 23:58:12 -0400

    fixed a weird thing

--- js/main.js
+++ js/main.js
@@ -5,22 +5,22 @@ var instance_sn = M.Sidenav.init(elem_sn);
 
 
 
-var elem_c = document.querySelector('.carousel');
+var elem = document.querySelector('.carousel');
 
-var instance_c = M.Carousel.init(elem_c, {
+var instance = M.Carousel.init(elem, {
     fullWidth: true,
     indicators: true
-});
+  });
 
 
 var elem = document.querySelector('.datepicker');
 var instance = M.Datepicker.init(elem);
 
-var elemSelectMeal = document.querySelector('select');
-var instanceSelectMeal = M.FormSelect.init(elemSelectMeal, options);
+// var elemSelectMeal = document.querySelector('select');
+// var instanceSelectMeal = M.FormSelect.init(elemSelectMeal, options);
 
 
 // var todd = document.querySelector("#today").valueAsDate = new Date();
 
 
-var instance_t = M.Tabs.init(el);
+// var instance_t = M.Tabs.init(el);
