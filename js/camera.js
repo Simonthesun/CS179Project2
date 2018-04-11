33b8cf982ac9e8ee5fefdf9723a239cc8bc52cca
Luke Xu <Lukexu@Lukes-MacBook-Pro-5.local>
2018-04-11 19:18:34 -0400

    not hot dog

--- js/camera.js
+++ js/camera.js
@@ -1,7 +1,15 @@
 var camButton = document.getElementById('camButton');
 var div = document.getElementById('foodInfo');
+var hd = document.getElementById('hotdog');
 
+//
 camButton.onclick = function() {
     div.className = "center-align scale-transition scale-in";
-    
+
+    camButton.className = "btn-floating disabled";
+
+    // adapted from https://stackoverflow.com/questions/29754029/javascript-animated-gif-to-remain-static-until-clicked
+    hd.src = hd.bln ? "images/hotdog.gif" : "images/hotdog2.jpeg";
+    hd.bln = !hd.bln;
+
 };
