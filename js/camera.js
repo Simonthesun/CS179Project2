4299f85a895a0468e086f6a0681c3c60924bf498
Luke Xu <Lukexu@Lukes-MacBook-Pro-5.local>
2018-04-11 19:54:48 -0400

    not hot dog with back button

--- js/camera.js
+++ js/camera.js
@@ -1,12 +1,27 @@
 var camButton = document.getElementById('camButton');
 var div = document.getElementById('foodInfo');
 var hd = document.getElementById('hotdog');
+var outButton = document.getElementById('outButton');
 
 //
 camButton.onclick = function() {
-    div.className = "center-align scale-transition scale-in";
 
+    div.className = "center-align scale-transition scale-in";
     camButton.className = "btn-floating disabled";
+    outButton.className = "btn-floating btn-large waves-effect waves-light red";
+
+    // adapted from https://stackoverflow.com/questions/29754029/javascript-animated-gif-to-remain-static-until-clicked
+    hd.src = hd.bln ? "images/hotdog.gif" : "images/hotdog2.jpeg";
+    hd.bln = !hd.bln;
+
+};
+
+outButton.onclick = function() {
+
+    div.className = "center-align scale-transition scale-out";
+    camButton.className = "btn-floating btn-large waves-effect waves-light blue";
+    outButton.className = "btn-floating disabled";
+
 
     // adapted from https://stackoverflow.com/questions/29754029/javascript-animated-gif-to-remain-static-until-clicked
     hd.src = hd.bln ? "images/hotdog.gif" : "images/hotdog2.jpeg";
