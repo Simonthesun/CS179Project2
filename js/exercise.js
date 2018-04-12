b9388ec9a8c31b8a743ee07f6bfab5eb738f8a0b
Simon Sun <ssun@college.harvard.edu>
2018-04-11 21:28:12 -0400

    added date option to exercise + fiddling with date data

--- js/exercise.js
+++ js/exercise.js
@@ -1,9 +1,9 @@
 var exercises = [];
 
-function storeExercise(exName, exIntensity, exTime) {
+function storeExercise(exName, exTime, exIntensity, exEnd) {
 	var today = new Date();
 
-	let thisExercise = {date:today, name:exName, intensity:exIntensity, time:exTime};
+	let thisExercise = {date:today, time:exTime, name:exName, intensity:exIntensity, end:exEnd};
 
 	if (exercises != null) {
 		exercises.push(thisExercise);
@@ -16,10 +16,18 @@ function storeExercise(exName, exIntensity, exTime) {
 
 function saveExercise() {
 	var activity = document.getElementById("activity").value;
+	var timeString = document.getElementById("time").value;
 	var intensity = document.getElementById("intensity").value;
-	var time = document.getElementById("time").value;
+	var duration = parseInt(document.getElementById("duration").value);
+	var startTimes = timeString.split(":");
+	var hour = parseInt(startTimes[0]);
+	var min = parseInt(startTimes[1]);
+	var time = new Date();
+	time.setHours(hour, min, 0);
+	var end = new Date(time.getTime());
+	end.setTime(time.getTime() + (duration * 60000));
 
-	storeExercise(activity, intensity, time);
+	storeExercise(activity, time, intensity, end);
 }
 
 function loadExercises() {
