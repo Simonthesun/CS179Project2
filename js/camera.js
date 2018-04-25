285a24aba91febbd96f853ad14bfc473ecabde58
Megan Ross <meganross@Megans-MacBook-Pro-3.local>
2018-04-25 17:14:05 -0400

    test

--- js/camera.js
+++ js/camera.js
@@ -33,15 +33,18 @@ outButton.onclick = function() {
 submitbutton.onclick = function() {
 
     var item = {}
-	console.log(document.getElementById("servingsize").value);
-	var curmeal = JSON.parse(localStorage.getItem("curMeal"));
+    console.log(item);
+	// console.log(document.getElementById("servingsize").value);
+    
+    var curmeal = localStorage.getItem("curMeal");
+    if (curmeal == null) {
+        curmeal = [];
+    }
 	item.name = "Hot Dog";
 	item.servingType = 1;
 	item.servingSize = 1;
 	item.carbCount = 10;
 	curmeal.push(item);
-	console.log("item is:", item);
-	console.log("meal is:", curmeal);
 	localStorage.setItem("curMeal", JSON.stringify(curmeal));
-    
+    console.log(curmeal);
 }
